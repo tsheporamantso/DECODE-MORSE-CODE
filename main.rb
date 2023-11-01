345fd7affa4270cdce40ca96d5692ecca9fa6071
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-11-01 07:54:00 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -32,3 +32,30 @@ MORSE_CODE = {
 def decode_character(str)
   MORSE_CODE[str]
 end
+
+# Decode a Morse code word
+def decode_word(word)
+  decoded = ''
+  arr_of_characters = word.split
+
+  arr_of_characters.each do |char|
+    decoded += decode_character(char)
+  end
+
+  decoded
+end
+
+def decode(message)
+  decoded_message = ''
+  word_codes = message.split('   ')
+
+  word_codes.each do |word|
+    decoded_message += if decoded_message == ''
+                         decode_word(word)
+                       else
+                         " #{decode_word(word)}"
+                       end
+  end
+
+  decoded_message
+end
